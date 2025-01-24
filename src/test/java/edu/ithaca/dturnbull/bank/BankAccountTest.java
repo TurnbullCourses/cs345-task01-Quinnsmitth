e82d6236577464d00dcb33ff4ee0b91ab7730442
Duncan Tasker <tasatree@gmail.com>
2025-01-24 10:32:02 -0500

    added tests for isEmailValid

--- src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
+++ src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
@@ -3,7 +3,6 @@ package edu.ithaca.dturnbull.bank;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-
 class BankAccountTest {
 
     @Test
@@ -14,7 +13,7 @@ class BankAccountTest {
     }
 
     @Test
-    void withdrawTest() throws InsufficientFundsException{
+    void withdrawTest() throws InsufficientFundsException {
         BankAccount bankAccount = new BankAccount("a@b.com", 200);
         bankAccount.withdraw(100);
 
@@ -23,11 +22,30 @@ class BankAccountTest {
     }
 
     @Test
-    void isEmailValidTest(){
-        assertTrue(BankAccount.isEmailValid( "a@b.com"));   // valid email address
-        assertFalse( BankAccount.isEmailValid(""));         // empty string
+    void isEmailValidTest() {
+        assertFalse(BankAccount.isEmailValid("")); // empty string
 
-        
+        // prefix test cases
+        assertTrue(BankAccount.isEmailValid("abc@mail.com")); // valid email address
+        assertTrue(BankAccount.isEmailValid("abc#def@mail.com")); // valid character in prefix
+        assertTrue(BankAccount.isEmailValid("abc-d@mail.com")); // valid character in prefix
+        assertTrue(BankAccount.isEmailValid("abc.def@mail.com")); // valid character in prefix
+        assertTrue(BankAccount.isEmailValid("abc_def@mail.com")); // valid character in prefix
+
+        assertFalse(BankAccount.isEmailValid("abc-@mail.com")); // invalid character at end of prefix
+        assertFalse(BankAccount.isEmailValid("abc..def@mail.com")); // two consecutive periods
+        assertFalse(BankAccount.isEmailValid(".abc@mail.com")); // period at start of prefix
+
+        // domain test cases
+        assertTrue(BankAccount.isEmailValid("abc.def@mail.cc")); // valid TLD
+        assertTrue(BankAccount.isEmailValid("abc.def@mail-archive.com")); // valid special character in domain
+        assertTrue(BankAccount.isEmailValid("abc.def@mail.org")); // valid TLD
+        assertTrue(BankAccount.isEmailValid("abc.def@mail.com")); // valid TLD
+
+        assertFalse(BankAccount.isEmailValid("abc.def@mail.c")); // TLD too short
+        assertFalse(BankAccount.isEmailValid("abc.def@mail#archive.com")); // invalid character in domain
+        assertFalse(BankAccount.isEmailValid("abc.def@mail..com")); // two consecutive periods
+        assertFalse(BankAccount.isEmailValid("abc.def@mail")); // no TLD
     }
 
     @Test
@@ -36,8 +54,8 @@ class BankAccountTest {
 
         assertEquals("a@b.com", bankAccount.getEmail());
         assertEquals(200, bankAccount.getBalance(), 0.001);
-        //check for exception thrown correctly
-        assertThrows(IllegalArgumentException.class, ()-> new BankAccount("", 100));
+        // check for exception thrown correctly
+        assertThrows(IllegalArgumentException.class, () -> new BankAccount("", 100));
     }
 
 }
